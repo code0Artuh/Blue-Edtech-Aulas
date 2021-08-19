f27e4237cfb9121402f52ba0132977a3d71f186c
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-08-19 16:22:39 -0300

    Update 10% do garçom.js

--- Exercicios de Fixação/10% do garçom.js
+++ Exercicios de Fixação/10% do garçom.js
@@ -9,5 +9,6 @@ console.log(pergunta);
 var A = 42.54
 var B = 0.10 
 var C = A * (1+B).toFixed(2); 
+var D = C.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
 
-console.log(`O total da sua conta incluindo os 10% do garçom é de R$ ${C}.`)
+console.log(`O total da sua conta incluindo os 10% do garçom é de ${D}.`)
