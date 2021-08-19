58e0d71fd8fdb2b0051c6cec0f09abf0ae46445d
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-08-19 17:22:49 -0300

    Update Cadastro com opções.js

--- Exercicios de Fixação/Cadastro com opções.js
+++ Exercicios de Fixação/Cadastro com opções.js
@@ -1,3 +1,4 @@
+//codigo
 let nomes = ["Artur", "Giovana", "Richard"];
 
 console.log(`
@@ -15,7 +16,53 @@ console.log(`
 Tem Certeza?
 1. ${definição[0]}
 2. ${definição[1]}`);
-let final = prompt("Digite o número da opção")
-if (final === '1'){
-    console.log(`Bem-Vindo(a) a Blue ${nomes[escolha-1]}`)
-  } else {console.log(`tente novamente`)}
+
+let final = prompt("Digite o número da opção");
+
+if (final === "1"){
+console.log (`Bem-Vindo(a) a Blue ${nomes[escolha-1]}`)} else{let nomes = ["Artur", "Giovana", "Richard"];
+
+console.log(`
+Escolha um Nome
+1. ${nomes[0]}
+2. ${nomes[1]}
+3. ${nomes[2]}
+`);
+
+let escolha = prompt("Digite o número da opção: ");
+console.log (`Seu Nome é ${nomes[escolha-1]}?`);
+
+var definição = ["Sim", "Não"];
+console.log(`
+Tem Certeza?
+1. ${definição[0]}
+2. ${definição[1]}`);
+
+let final = prompt("Digite o número da opção");
+
+if (final === "1"){
+console.log (`Bem-Vindo(a) a Blue ${nomes[escolha-1]}`)} else{
+console.log("Ultima chance!!!")
+  let nomes = ["Artur", "Giovana", "Richard"];
+
+console.log(`
+Escolha um Nome
+1. ${nomes[0]}
+2. ${nomes[1]}
+3. ${nomes[2]}
+`);
+
+let escolha = prompt("Digite o número da opção: ");
+console.log (`Seu Nome é ${nomes[escolha-1]}?`);
+
+var definição = ["Sim", "Não"];
+console.log(`
+Tem Certeza?
+1. ${definição[0]}
+2. ${definição[1]}`);
+
+let final = prompt("Digite o número da opção");
+
+if (final === "1"){
+console.log (`Bem-Vindo(a) a Blue ${nomes[escolha-1]}`)} else{"Por Favor, Tente novamente"};};
+};
