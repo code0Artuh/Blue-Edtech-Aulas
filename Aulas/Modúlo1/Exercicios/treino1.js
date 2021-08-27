a3cbdff4edd95743f7c521a662db9cfaa2fc9fa1
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-08-27 16:08:05 -0300

    main

---
+++ Aulas/Modúlo1/Exercicios/treino1.js
@@ -0,0 +1,19 @@
+/*Exercício Treino 1 - Escreva uma função que recebe dois parâmetros (números) e 
+imprime o menor dos dois. Se eles forem iguais, imprima que são valores idênticos.*/
+
+let a = 0;
+
+function print() {
+  let b = prompt("digite um valor");
+  let c = prompt("digite um valor");
+  if (c > b) {
+    console.log(`${c} é maior`);
+  } else if (c == b) {
+    console.log("os valores são identicos");
+  } else if (c < b) {
+    console.log(`${b} é maior`);
+  }
+}
+if (a === 0) {
+  print();
+}
