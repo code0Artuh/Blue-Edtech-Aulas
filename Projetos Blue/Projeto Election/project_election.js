818c694b20f5ca19a93fc70e6b82eb9fc963186e
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-09-04 07:30:02 -0300

    main

--- Projetos Blue/Projeto Election/project_election.js
+++ Projetos Blue/Projeto Election/project_election.js
@@ -1,6 +1,8 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-console.log("Bem-vindo as eleições da Blue,\nMenores de 18 anos não podem votar\nMaiores de 50 anos tem voto opcional")
+console.log(
+  "Bem-vindo as eleições da Blue,\nMenores de 18 anos não podem votar\nMaiores de 50 anos tem voto opcional"
+);
 //inicio pedindo quantidade só por interação, no fim do codigo eu dou a opção para receber votos.
 let qtd = +prompt("Quantas pessoas iram votar: ");
 
@@ -14,112 +16,118 @@ let cand3 = 0;
 let nulo = 0;
 let branco = 0;
 //a função que contabiliza os votos.
-function candidato(){
-    console.log("escolha o numero referente ao seu voto\ncandidato1 = 1\ncandidato2 = 2\ncandidato3 = 3\nnulo = 4\nbranco = 5")
-    let b = +prompt("Digite o numero: ");
-    if(b === 1){
-        cand1++;
-    }else if(b === 2){
-        cand2++;
-    }else if(b === 3){
-        cand3++;
-    }else if(b === 4){
-        nulo++;
-    }else if(b === 5){
-        branco++;
-    }
+function candidato() {
+  console.log(
+    "escolha o numero referente ao seu voto\ncandidato1 = 1\ncandidato2 = 2\ncandidato3 = 3\nnulo = 4\nbranco = 5"
+  );
+  let b = +prompt("Digite o numero: ");
+  if (b === 1) {
+    cand1++;
+  } else if (b === 2) {
+    cand2++;
+  } else if (b === 3) {
+    cand3++;
+  } else if (b === 4) {
+    nulo++;
+  } else if (b === 5) {
+    branco++;
+  }
 }
 //função que entrega todos os resultados
-function resultados(){
-    console.log("Exibindo os resultados:\n");
-    console.log(`Candidato 1 recebeu ${cand1} votos.\n`);
-    console.log(`Candidato 2 recebeu ${cand2} votos.\n`);
-    console.log(`Candidato 3 recebeu ${cand3} votos.\n`);
-    console.log(`Tivemos ${nulo} votos nulos.\n`);
-    console.log(`Tivemos ${branco} votos brancos.\n`);
-    if( cand1 > cand2 && cand3 < cand1) {
-        console.log(`Com ${cand1} votos, o Candidato 1 é o vencedor das eleições da Blue`)
-    }else if( cand2 > cand3 && cand1 < cand2) {
-        console.log(`Com ${cand2} votos, o Candidato 2 é o vencedor das eleições da Blue`)
-    }else if( cand3 > cand1 && cand2 < cand3) {
-        console.log(`Com ${cand3} votos, o Candiddato 3 é o vencedor das eleições da Blue`)
-    }else if(nulo > cand1 && nulo > cand2 && nulo > cand3) {
-        console.log("ninguem ganhou as eleições")
-    }else if(branco > cand1 && branco > cand2 && branco > cand3) {
-        console.log("ninguem ganhou as eleições")
-    }else if(cand1 === cand2 && cand1 === cand3) {
-        console.log("temos um empate")
-    }else if(cand1 === cand2 && cand1 > cand3) {
-        console.log("temos um empate")
-    }else if(cand1 === cand3 && cand1 > cand2) {
-        console.log("temos um empate")
-    }else if(cand2 === cand3 && cand2 > cand1) {
-        console.log("temos um empate")
-    }else if(cand2 === cand1 && cand2 > cand3) {
-        console.log("temos um empate")
-    }else if (cand3 === cand2 && cand3 > cand1){
-        console.log("temos um empate")
-    }else if (cand3 === cand2 && cand3 > cand2){
-        console.log("temos um empate")
-    }
+function resultados() {
+  console.log("Exibindo os resultados:\n");
+  console.log(`Candidato 1 recebeu ${cand1} votos.\n`);
+  console.log(`Candidato 2 recebeu ${cand2} votos.\n`);
+  console.log(`Candidato 3 recebeu ${cand3} votos.\n`);
+  console.log(`Tivemos ${nulo} votos nulos.\n`);
+  console.log(`Tivemos ${branco} votos brancos.\n`);
+  if (cand1 > cand2 && cand3 < cand1) {
+    console.log(
+      `Com ${cand1} votos, o Candidato 1 é o vencedor das eleições da Blue`
+    );
+  } else if (cand2 > cand3 && cand1 < cand2) {
+    console.log(
+      `Com ${cand2} votos, o Candidato 2 é o vencedor das eleições da Blue`
+    );
+  } else if (cand3 > cand1 && cand2 < cand3) {
+    console.log(
+      `Com ${cand3} votos, o Candidato 3 é o vencedor das eleições da Blue`
+    );
+  } else if (nulo > cand1 && nulo > cand2 && nulo > cand3) {
+    console.log("ninguem ganhou as eleições");
+  } else if (branco > cand1 && branco > cand2 && branco > cand3) {
+    console.log("ninguem ganhou as eleições");
+  } else if (cand1 === cand2 && cand1 === cand3) {
+    console.log("temos um empate");
+  } else if (cand1 === cand2 && cand1 > cand3) {
+    console.log("temos um empate");
+  } else if (cand1 === cand3 && cand1 > cand2) {
+    console.log("temos um empate");
+  } else if (cand2 === cand3 && cand2 > cand1) {
+    console.log("temos um empate");
+  } else if (cand2 === cand1 && cand2 > cand3) {
+    console.log("temos um empate");
+  } else if (cand3 === cand2 && cand3 > cand1) {
+    console.log("temos um empate");
+  } else if (cand3 === cand2 && cand3 > cand2) {
+    console.log("temos um empate");
+  }
 }
 
-for(var i = 0; i < qnt; i++) {
-    voto()
+for (var i = 0; i < qnt; i++) {
+  voto();
 }
 
-
-function voto(){
-
-let a = 0;
-if (a === 0){
+function voto() {
+  let a = 0;
+  if (a === 0) {
     autorizaVoto();
-}
-//função que verifica o usuario e libera pra votar
-function autorizaVoto(){
+  }
+  //função que verifica o usuario e libera pra votar
+  function autorizaVoto() {
     let ok = 0;
-let choose = 0;
-let no = 0;
+    let choose = 0;
+    let no = 0;
     let idade = +prompt("Digite o ano do seu nascimento com 4 digitos: ");
-    if(idade > 2003){
-        no++;
-        console.log("\nvoto negado\n");
-    }else if(idade < 2004 && idade >
-        1971){
-        ok++;
-        console.log("\nvoto obrigatorio\n");
-    }else if(idade < 1972 ){
-        choose++;
-        console.log("\nvoto opcional\n")
+    if (idade > 2003) {
+      no++;
+      console.log("\nvoto negado\n");
+    } else if (idade < 2004 && idade > 1971) {
+      ok++;
+      console.log("\nvoto obrigatorio\n");
+    } else if (idade < 1972) {
+      choose++;
+      console.log("\nvoto opcional\n");
     }
     votação();
 
-    function votação(){
-        if(no === 1){
-            console.log("você não pode votar")
-        }else if (ok === 1) {
-            candidato();
-        }else if(choose === 1){
-            let k = +prompt("você gostaria de votar? 1-sim ou 2-não: ")
-            if(k === 1){
-                candidato();
-            }
+    function votação() {
+      if (no === 1) {
+        console.log("você não pode votar");
+      } else if (ok === 1) {
+        candidato();
+      } else if (choose === 1) {
+        let k = +prompt("você gostaria de votar? 1-sim ou 2-não: ");
+        if (k === 1) {
+          candidato();
         }
+      }
     }
-}
-
+  }
 }
 
 let l = 0;
-if(l === 0){
-    mais();
+if (l === 0) {
+  mais();
 }
 //aqui eu pergunto ao usuario se existe mais pessoas para votar e repito todo o codigo caso a resposta seja sim
 //caso seja não eu finalizo a contabilização de votos e entrego os resultados
-function mais(){
-let prg = +prompt("Mais alguem pra votar? 1-sim ou 2-não: ")
-if(prg === 1){
+function mais() {
+  let prg = +prompt("Mais alguem pra votar? 1-sim ou 2-não: ");
+  if (prg === 1) {
     voto();
     mais();
-}else{resultados();}
+  } else {
+    resultados();
+  }
 }
