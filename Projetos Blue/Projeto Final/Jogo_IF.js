953aebc0d76be5cdfe28f2f6b657e23031528d7c
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-09-07 22:55:20 -0300

    main

--- Projetos Blue/Projeto Final/Jogo_IF.js
+++ Projetos Blue/Projeto Final/Jogo_IF.js
@@ -1,15 +1,1 @@
 const prompt = require('prompt-sync')();
-
-
-console.log("Bem vindos ao projeto final1");
-
-
-
-console.log("Bem vindos");
-console.log("Bem vindos ao projeto final");
-
-console.log("Bruno aqui")
-console.log("Bem vindos ao projeto final1");
-
-
-console.log("Bem vindos2222222222");
