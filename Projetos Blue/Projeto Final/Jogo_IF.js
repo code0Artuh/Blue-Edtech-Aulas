7d36c31193b52e4e0735714c3b643055b83f3e1a
Bruno Sallum <87883544+Thesent@users.noreply.github.com>
2021-09-07 22:35:43 -0300

    Bruno aqui

--- Projetos Blue/Projeto Final/Jogo_IF.js
+++ Projetos Blue/Projeto Final/Jogo_IF.js
@@ -1,3 +1,13 @@
 const prompt = require('prompt-sync')();
 
+<<<<<<< Updated upstream
 console.log("Bem vindos ao projeto final1");
+=======
+<<<<<<< HEAD
+console.log("Bem vindos ao projeto final");
+
+console.log("Bruno aqui")
+=======
+console.log("Bem vindos ao projeto final1");
+>>>>>>> 747b6bbdbca3a91ddd94764b1b9f21d72b83eb5b
+>>>>>>> Stashed changes
