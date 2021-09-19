d1fe7d5308c6a23cb7a3ff86816150e471a1c64e
Code0Artuh <87547578+code0Artuh@users.noreply.github.com>
2021-09-19 19:20:38 -0300

    main

---
+++ Aulas/Modúlo1/Exercicios/promise.js
@@ -0,0 +1,22 @@
+/*exercícios para hoje:
+1-) Primeiramente, crie um arquivo chamado scripts.js. Neste arquivo,
+ iremos criar uma função de soma que precisará ter como resultado um número par.
+  Para isso, você precisará criar os seguintes callbacks:
+
+callbackSucesso() = trará uma mensagem de sucesso, dizendo que a operação foi concluída com sucesso e que o número somado é par.
+callBackError() = trará uma mensagem de erro, dizendo que a operação falhou, trazendo um número impar (resultado da soma).
+Utilize a função anterior para criar uma promise que tenha:
+
+then: uma mensagem de sucesso, informando que a operação foi concluída com sucesso, dizendo se é par ou ímpar e trazendo o número;
+catch: uma mensagem de erro, informando que a operação possui erro, dizendo se é par ou ímpar e trazendo o número.
+FIM DO EXERCÍCIO 1
+[14:29]
+2-) Crie uma função que após 5 segundos, dobre o resultado do número passado como parâmetro. Essa função deverá retornar uma promise. 
+Use o setTimeOut como temporizador.
+
+Crie uma função assíncrona que irá receber um parâmetro x. Crie três variáveis: a, b e c dentro do corpo da função.
+ Cada uma dessas variáveis receberá um await da função do passo anterior. Você deve passar como parâmetros dessa função os número 10, 20 e 30, respectivamente. 
+ Retorne a soma de todos esses elementos, inclusive o parâmetro da função assíncrona
+[14:29]
+3-)  Crie um programa  que valide através de promises a idade de um usuário. Caso ele seja maior de idade  
+exibir:" Parabéns cara você já é maior de idade." Caso contrário:  "Você precisa comer ainda muito feijão com arroz!!".*/
